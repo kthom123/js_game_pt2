160ca780e608de674b69a4de330b1367d5ebf85f
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-04 17:14:44 +0900

    added slider

--- script.js
+++ script.js
@@ -23,19 +23,40 @@ class Layer {
         this.height = 700;
         this.x2 = this.width;
         this.image = image;
-        this.speedModifier = speedModifierl
-        this.speed = gameSpeed * this.speed
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed * this.speedModifier;
+    }
+    update(){
+        this.speed = gameSpeed * this.speedModifier;
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if (this.x2 <= -this.width){
+          this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
     }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x, 0);
-    ctx.drawImage(backgroundLayer4, x2, 0);
-    if (x < -2400) x = 2400 + x2 - gameSpeed;
-    else x -= gameSpeed;
-    if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-    else x2 -= gameSpeed;
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
     requestAnimationFrame(animate);
 };
 animate();
